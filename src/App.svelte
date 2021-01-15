34ec3b43fe751655e8bcf9ac0d073b3236b547a1
goodmite <sandeepkgupta007@gmail.com>
2021-01-15 14:30:45 +0530

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let name;
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <div>
+        hello
+    </div>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="text/scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+
+  :global {
+    body {
+      background: red;
+    }
+  }
+</style>
